7debb85c25133e92ece07888dee773fe6982deaa
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2022-05-30 13:31:59 +0300

    fixed required fields

--- microtools/microinvoicer/models.py
+++ microtools/microinvoicer/models.py
@@ -135,8 +135,8 @@ class TimeInvoice(models.Model):
     unit = models.CharField(max_length=2, choices=InvoicingUnits.choices)
     unit_rate = models.DecimalField(max_digits=16, decimal_places=2)
 
-    attached_cost = models.DecimalField(max_digits=16, decimal_places=2, null=True)
-    attached_description = models.CharField(max_length=LONG_TEXT, null=True)
+    attached_cost = models.DecimalField(max_digits=16, decimal_places=2, blank=True, null=True)
+    attached_description = models.CharField(max_length=LONG_TEXT, blank=True, null=True)
 
     issue_date = models.DateField()
     quantity = models.IntegerField()
