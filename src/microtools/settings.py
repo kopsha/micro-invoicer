547c554bf97b31b1f765ffc37afa7b67c6d408cc
Florin Ciurcanu <kopsha@users.noreply.github.com>
2024-02-25 20:30:20 +0200

    Better deployment with helm & k8s (#15)
    * Prepared some environment isolation of values

--- src/microtools/settings.py
+++ src/microtools/settings.py
@@ -54,9 +54,13 @@ TEMPLATES = [
 WSGI_APPLICATION = "microtools.wsgi.application"
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join("/", "app", "data", "db.sqlite3"),
-    }
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+    },
 }
 
 AUTH_PASSWORD_VALIDATORS = [
